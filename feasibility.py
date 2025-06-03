f349a94b2ee8ccd9cb83a1e77895071f22a895f6
Harry Thawley <hmthawley@gmail.com>
2025-06-03 15:44:39 +0200

    remove cross/mut

--- feasibility.py
+++ feasibility.py
@@ -10,7 +10,6 @@ Created on Tue Jun  3 14:10:01 2025
 #   * Re-crossover with elite for very infeasible solutions
 # =============================================================================
 
-from crossover_mutation import crossover, mutation
 
 def feasibility(*args, **kwargs):
     """
