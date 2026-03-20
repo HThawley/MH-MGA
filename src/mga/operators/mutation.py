fac20ae4e6dab67e1b447241a6137f7023287208
Harry Thawley <u6942852@anu.edu.au>
2026-03-20 14:11:41 +1100

    clean up names amend

--- src/mga/operators/mutation.py
+++ src/mga/operators/mutation.py
@@ -3,7 +3,7 @@ from numba import njit
 
 # API functions
 @njit
-def mutate_gaussian_population_mixed(points, sigma, indpb, rng, integrality, booleanality, startidx=0):
+def mutate_gaussian_population_mixed(points, mutation_sigma, mutation_prob, rng, integrality, booleanality, startidx=0):
     """
     Mutate individuals in a population
     Compatible with mixed dtypes
@@ -11,17 +11,17 @@ def mutate_gaussian_population_mixed(points, sigma, indpb, rng, integrality, boo
     for i in range(points.shape[0]):
         for j in range(startidx, points.shape[1]):
             for k in range(points.shape[2]):
-                if rng.random() < indpb:
+                if rng.random() < mutation_prob:
                     if booleanality[k]:
-                        points[i, j, k] = _mutate_bool(points[i, j, k], sigma[k], rng)
+                        points[i, j, k] = _mutate_bool(points[i, j, k], mutation_sigma[k], rng)
                     elif integrality[k]:
-                        points[i, j, k] = _mutate_int(points[i, j, k], sigma[k], rng)
+                        points[i, j, k] = _mutate_int(points[i, j, k], mutation_sigma[k], rng)
                     else:
-                        points[i, j, k] = _mutate_float(points[i, j, k], sigma[k], rng)
+                        points[i, j, k] = _mutate_float(points[i, j, k], mutation_sigma[k], rng)
 
 
 @njit
-def mutate_gaussian_population_float(points, sigma, indpb, rng, startidx=0):
+def mutate_gaussian_population_float(points, mutation_sigma, mutation_prob, rng, startidx=0):
     """
     Mutate individuals in a population
     Compatible only with float-only
@@ -29,58 +29,58 @@ def mutate_gaussian_population_float(points, sigma, indpb, rng, startidx=0):
     for i in range(points.shape[0]):
         for j in range(startidx, points.shape[1]):
             for k in range(points.shape[2]):
-                if rng.random() < indpb:
-                    points[i, j, k] = _mutate_float(points[i, j, k], sigma[k], rng)
+                if rng.random() < mutation_prob:
+                    points[i, j, k] = _mutate_float(points[i, j, k], mutation_sigma[k], rng)
 
 
 @njit
-def mutate_gaussian_niche_mixed(niche, sigma, indpb, rng, integrality, booleanality, startidx=0):
+def mutate_gaussian_niche_mixed(niche, mutation_sigma, mutation_prob, rng, integrality, booleanality, startidx=0):
     """
     Mutate individuals in a niche
     Compatible with mixed dtypes
     """
     for j in range(startidx, niche.shape[0]):
         for k in range(niche.shape[1]):
-            if rng.random() < indpb:
+            if rng.random() < mutation_prob:
                 if booleanality[k]:
-                    niche[j, k] = _mutate_bool(niche[j, k], sigma[k], rng)
+                    niche[j, k] = _mutate_bool(niche[j, k], mutation_sigma[k], rng)
                 elif integrality[k]:
-                    niche[j, k] = _mutate_int(niche[j, k], sigma[k], rng)
+                    niche[j, k] = _mutate_int(niche[j, k], mutation_sigma[k], rng)
                 else:
-                    niche[j, k] = _mutate_float(niche[j, k], sigma[k], rng)
+                    niche[j, k] = _mutate_float(niche[j, k], mutation_sigma[k], rng)
 
 
 @njit
-def mutate_gaussian_niche_float(niche, sigma, indpb, rng, startidx=0):
+def mutate_gaussian_niche_float(niche, mutation_sigma, mutation_prob, rng, startidx=0):
     """
     Mutate individuals in a niche
     Compatible only with float-only
     """
     for j in range(startidx, niche.shape[0]):
         for k in range(niche.shape[1]):
-            if rng.random() < indpb:
-                niche[j, k] = _mutate_float(niche[j, k], sigma[k], rng)
+            if rng.random() < mutation_prob:
+                niche[j, k] = _mutate_float(niche[j, k], mutation_sigma[k], rng)
 
 
 # private helper functions
 
 
 @njit
-def _mutate_float(item, sigma, rng):
+def _mutate_float(item, mutation_sigma, rng):
     """gaussian mutation for single variable"""
-    return rng.normal(item, sigma)
+    return rng.normal(item, mutation_sigma)
 
 
 @njit
-def _mutate_int(item, sigma, rng):
+def _mutate_int(item, mutation_sigma, rng):
     """Integer mutation for single variable. Returns integer-valued float"""
-    return round(rng.normal(item, sigma))
+    return round(rng.normal(item, mutation_sigma))
 
 
 @njit
-def _mutate_bool(item, sigma, rng):
+def _mutate_bool(item, mutation_sigma, rng):
     """Boolean mutation for single variable. Returns boolean-valued float"""
-    if abs(rng.normal(0, sigma)) <= rng.random():
+    if abs(rng.normal(0, mutation_sigma)) <= rng.random():
         return item
     else:
         return 1.0 - item
